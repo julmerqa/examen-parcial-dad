2ceb554656bf4b12f1a10c7ff67fe121d0e2c7b6
julmerqa <quispejulmer171@gmail.com>
2024-12-01 12:01:47 -0500

    Config-Server

---
+++ config-server/src/test/java/jqa/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jqa.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
